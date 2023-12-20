787d65abe3b51224bbe1fcde5946109837663978
Kavindu Wijegunasekara <110486479+Kavinduprabasara@users.noreply.github.com>
2023-12-20 13:43:54 +0530

    app created
    feat : new core

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="text-red-200 flex min-h-screen flex-col items-center justify-between p-24">
+      Main Page
+    </main>
+  )
+}
